296a51372fc4067c661e74ae12f49cf8a3f043ed
VIVEK S M <138435001+viveksm2003@users.noreply.github.com>
2024-07-09 19:50:49 +0530

    Add files via upload

---
+++ resume builder/resume.js
@@ -0,0 +1,9 @@
+// script.js
+function toggleVisibility(id) {
+    var element = document.getElementById(id);
+    if (element.style.display === "none" || element.style.display === "") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
